127147f9773af03a096132af935bd1f2c0523e50
Kwame Dalrymple <neokwame@tutanota.com>
2020-01-14 21:24:50 +0000

    stupid coaching done

---
+++ app/controllers/question_controller.rb
@@ -0,0 +1,21 @@
+class QuestionController < ApplicationController
+  # def index
+  #   @ask = ask
+  # end
+
+  def ask
+    @ask = params[:question]
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+        @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+  end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'question#ask', as: :ask
+  get 'answer', to: 'question#answer', as: :answer
+  # post 'ask', to: 'question#answer', as: :ask
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
